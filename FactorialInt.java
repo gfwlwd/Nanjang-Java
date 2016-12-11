a74f5360424a0e3a3c36e588ec7afbb6f4260135
gfwlwd <gfwlwd@hotmail.com>
2016-12-11 22:48:17 +0100

    FactorialInt

---
+++ FactorialInt.java
@@ -0,0 +1,22 @@
+
+public class FactorialInt {
+
+	public static void main(String[] args) {
+		createFactural();
+	}
+	public static void createFactural() {
+		int max = Integer.MAX_VALUE;
+		long factorial =  1;
+		// n! = 1*2*3...*n
+		for (int i = 1; ; i++) {
+			factorial = factorial * i;
+			
+			if (factorial > max ) {
+				System.out.println("The Factorial of " + i + " is out of range. ");
+				break;
+			} else {
+				System.out.println("The Factorial of " + i + " is " + factorial);
+			}
+		}
+	}
+}
