99a9d4b15f1ce3768cd424da117a595c0fd2c9d0
gfwlwd <gfwlwd@hotmail.com>
2016-12-12 22:51:20 +0100

    Return the factorial of the given integer, recursively

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,29 @@
 public class Factorial {
-	 // Print factorial of n
-	   public static void main(String[] args) {  
-		  // Set an initial breakpoint at this statement
-		   createFactural(17);
-	      //becomes negative number as int when n = 17
-	       
-	   }
-	   public static void createFactural(int n) {
-long factorial =  1;
-	      // n! = 1*2*3...*n
-	      for (int i = 1; i <= n; i++) {
-	         factorial = factorial * i;
-	      }
-	      System.out.println("The Factorial of " + n + " is " + factorial);
-	   }
+	// Print factorial of n
+	public static void main(String[] args) {  
+		createFactorial(17);
+		//becomes negative number as int when n = 17
+		System.out.println(factorial(5));
+		System.out.println(factorialShort(16));
+	}
+	public static void createFactorial(int n) {
+		long factorial =  1;
+		// n! = 1*2*3...*n
+		for (int i = 1; i <= n; i++) {
+			factorial = factorial * i;
+		}
+		System.out.println("The Factorial of " + n + " is " + factorial);
+	}
+	// Return the factorial of the given integer, recursively
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;   // base case
+		} else {
+			return n * factorial(n-1);  // call itself	
+		}
+	}
 
+	public static int factorialShort(int n) {
+		return (n == 0) ? 1 : n*factorialShort(n-1);
+	}
 }
