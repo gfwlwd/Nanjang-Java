f783f7b77cf2523068d07a87024f30c1a303c1f0
gfwlwd <gfwlwd@hotmail.com>
2017-03-28 23:58:02 +0200

    update

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,42 +5,49 @@ public class SelectionSort {
 	public static void main(String[] args) {
 		selectionSort(new int[] {0,2,4,1,3});
 		selectionSort(new int[] {5,90,35,45,150,3});
+		selectionSort(new int[] {6,5,4,3,2,1});
+		selectionSort(new int[] {1,2,3,4,5,6,7});
+
 
 	}
 	public static void selectionSort(int[] array) {
 		//prints array
 		System.out.println("Array before Selection Sort");
-		for(int i=0; i < array.length; i++){
+		/*for(int i=0; i < array.length; i++){
 			System.out.print(array[i] + " ");
 		}
-		System.out.println();
+		System.out.println();*/
 		int maxIndex = array.length;
 
-		for (int i = 0; i < array.length; i++) {  	// loop form index 0 till end              
+		for (int i = 0; i < array.length/2+1; i++) {  	// loop form index 0 till end              
 
 			int minIndex = i;
-
-			for (int j = i + 1; j < maxIndex; j++) { 
+			int max= 0;
+			int maxIndexNo = 0;
+			System.out.println(Arrays.toString(array));
+			for (int j = i ; j < maxIndex; j++) { 
 				if (array[j] < array[minIndex]) { 
-					//System.out.println(j);
+					//System.out.println("min = " + array[j]);
 					minIndex = j;
 				}
-
+				else if (array[j] > max) { 
+					//System.out.println(" max = " + array[j]);
+					max = array[j];
+					maxIndexNo = j;
+				}
 			}
-			 for (int m = maxIndex; m > minIndex; m--) {
-				 System.out.println(array[m-1]);
-				 
-			 }
-			System.out.println(Arrays.toString(array));
-			System.out.println("minindex "+(minIndex)+" = " + array[minIndex]);
-			System.out.println("maxindex "+(maxIndex)+" = " + array[maxIndex-1]);
-			
-			//Swap numbers in the given array
+
 			int temp = array[i];
 			array[i] = array[minIndex];
 			array[minIndex] = temp;	
+			
+			if (i != 0) { // wont work if i=0 and highest value is at 0
+				int temp2 = array[maxIndex-1];
+				array[maxIndex-1] = array[maxIndexNo];
+				array[maxIndexNo]= temp2;
+			}
 		}
-		//prints array
+		//prints array as string
 		System.out.println("Array after Selection Sort");
 		for(int i=0; i < array.length; i++){
 			System.out.print(array[i] + " ");
@@ -49,14 +56,12 @@ public class SelectionSort {
 		System.out.println();
 	}
 }
-/*In pseudocode, the algorithm is the following:
+// TODO:
+// for (int i = 0; i < array.length/2+1; i++) { 
+// int start = array.length;
+//  for (start=i; start < end; start++ ) {
+// check max and min
+// } // end for
+//
+//} // end for
 
-    Initialize a minIndex index variable which points to the beginning of the array (where the minimum values will be placed).
-    Initialize a min variable of the array item type with the value at minIndex. This variable will contain the minimum item value in the array (it may not contain it at first, but will eventually do so).
-    Initialize a newMinIndex index variable with minIndex+1. This variable contains the location where the found minimum items will be placed during the pass.
-    For each item in the array from the item at minIndex+1 to the end of the array, if the item is less or equal to min do:
-        If the item is less than min, assign the item to min and assign minIndex to newMinIndex.
-        Swap the item at the current location with the item at newMinIndex.
-        Increase newMinIndex by one. 
-    Assign newMinIndex to minIndex.
-    If minIndex is still inside the array, jump to step 2. */
